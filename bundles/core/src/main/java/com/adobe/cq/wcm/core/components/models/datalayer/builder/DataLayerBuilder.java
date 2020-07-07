03a3d011da7b6ec7403bf68c6d3de9beaa71b364
Kyle Giovannetti <kyle.giovannetti@dal.ca>
2020-07-06 23:25:15 -0300

    Remove AssetDataLayerSupplier

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/datalayer/builder/DataLayerBuilder.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/models/datalayer/builder/DataLayerBuilder.java
@@ -22,7 +22,6 @@ import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.Com
 import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.IDRequiredDataBuilderImpl;
 import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.ImageComponentDataBuilderImpl;
 import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.PageDataBuilderImpl;
-import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.supplier.AssetDataLayerSupplier;
 import com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.supplier.DataLayerSupplierImpl;
 import com.adobe.cq.wcm.core.components.models.datalayer.AssetData;
 import com.adobe.cq.wcm.core.components.models.datalayer.ComponentData;
@@ -30,7 +29,17 @@ import com.adobe.cq.wcm.core.components.models.datalayer.ContainerData;
 import com.adobe.cq.wcm.core.components.models.datalayer.ImageData;
 import com.adobe.cq.wcm.core.components.models.datalayer.PageData;
 import com.day.cq.dam.api.Asset;
+import com.day.cq.tagging.TagConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.sling.api.resource.ValueMap;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.adobe.cq.wcm.core.components.internal.models.v1.datalayer.builder.supplier.DataLayerSupplier.EMPTY_SUPPLIER;
 
@@ -102,7 +111,25 @@ public final class DataLayerBuilder {
      * @return A new AssetDataBuilder pre-initialized using the DAM asset metadata.
      */
     public static AssetDataBuilder forAsset(@NotNull final Asset asset) {
-        return new AssetDataBuilderImpl(new AssetDataLayerSupplier(asset));
+        return DataLayerBuilder.forAsset()
+            .withId(asset::getID)
+            .withFormat(asset::getMimeType)
+            .withUrl(asset::getPath)
+            .withLastModifiedDate(() -> new Date(
+                Optional.of(asset.getLastModified())
+                    .filter(lastMod -> lastMod > 0)
+                    .orElseGet(() -> Optional.ofNullable(asset.adaptTo(ValueMap.class))
+                        .map(vm -> vm.get(JcrConstants.JCR_CREATED, Calendar.class))
+                        .map(Calendar::getTimeInMillis)
+                        .orElse(0L))))
+            .withTags(() ->
+                Optional.ofNullable(asset.getMetadataValueFromJcr(TagConstants.PN_TAGS))
+                    .filter(StringUtils::isNotEmpty)
+                    .map(tagsValue -> tagsValue.split(","))
+                    .map(Arrays::stream)
+                    .orElseGet(Stream::empty)
+                    .filter(StringUtils::isNotEmpty)
+                    .toArray(String[]::new));
     }
 
     /**
